9b4cbd75b2a9812a94c5828031ec036acf798671
UnFarel <rayc0r379@gmail.com>
2025-06-11 00:58:20 +0300

    main init

--- predict_accessibility.py
+++ predict_accessibility.py
@@ -84,5 +84,8 @@ def predict_access():
     return jsonify(response)
 
 
+# === Запуск приложения ===
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render задаёт PORT
+    app.run(host="0.0.0.0", port=port)
